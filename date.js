3a06fd1dae13ab8063ef02afe9c70248f9bfbf3a
NithishKumar <nithish.thiruselvam@focusrtech.com>
2022-12-29 13:16:01 +0530

    new files are added

--- date.js
+++ date.js
@@ -1,43 +1,43 @@
-// var d = new Date().getDate();
-// var m = new Date().getMonth();
-// var y = new Date().getFullYear();
-// var day = new Date().getDay();
-// var hr =  new Date().getHours() - 12
-// var min = new Date().getMinutes()
-// var str = "";
-// switch (day) {
-//   case 0:
-//     str += "Sun";
-//     break;
-//   case 1:
-//     str += "Mon";
-//     break;
-//   case 2:
-//     str += "Tue";
-//     break;
-//   case 3:
-//     str += "Wed";
-//     break;
-//   case 4:
-//     str += "Thu";
-//     break;
-//   case 5:
-//     str += "Fri";
-//     break;
-//   case 6:
-//     str += "Sat";
-//     break;
-//   default:
-//     break;
-// }
-// // console.log(d + "/" + m + "/" + y + " - " + str + " - " +hr + ":" + min );
+// // var d = new Date().getDate();
+// // var m = new Date().getMonth();
+// // var y = new Date().getFullYear();
+// // var day = new Date().getDay();
+// // var hr =  new Date().getHours() - 12
+// // var min = new Date().getMinutes()
+// // var str = "";
+// // switch (day) {
+// //   case 0:
+// //     str += "Sun";
+// //     break;
+// //   case 1:
+// //     str += "Mon";
+// //     break;
+// //   case 2:
+// //     str += "Tue";
+// //     break;
+// //   case 3:
+// //     str += "Wed";
+// //     break;
+// //   case 4:
+// //     str += "Thu";
+// //     break;
+// //   case 5:
+// //     str += "Fri";
+// //     break;
+// //   case 6:
+// //     str += "Sat";
+// //     break;
+// //   default:
+// //     break;
+// // }
+// // // console.log(d + "/" + m + "/" + y + " - " + str + " - " +hr + ":" + min );
 
-// if(m === 11){
-//   m += 1
-// }
-// const date = y + "/" + m + "/" + d
-//  console.log(date)
-const func = require("./new")
+// // if(m === 11){
+// //   m += 1
+// // }
+// // const date = y + "/" + m + "/" + d
+// //  console.log(date)
+// const func = require("./new")
 
 
-console.log(func())
+// console.log(func())
